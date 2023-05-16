050f0e7fbf87cbb054a0f35fb590b3a346314e47
Nicolas <rojonicolasdev@gmail.com>
2023-05-16 15:38:46 -0300

    Added validation middlewares

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,32 +8,42 @@ const validationsMiddlewares = new ValidationsMiddlewares();
 const UserController = require('../controllers/userController');
 const userController = new UserController();
 
-router.get('/', userController.getAll);
+router.get('/',
+    [
+        validationsMiddlewares.validateJWT
+    ], 
+    userController.getAll);
 
 router.post('/',
     [
+        validationsMiddlewares.validateJWT,
         check('firstName', 'First name is required').not().isEmpty(),
         check('lastName', 'Last name is required').not().isEmpty(),
         check('dni', 'Dni is required').not().isEmpty(),
         check('password', 'Password is required').not().isEmpty(), 
         check('email', 'Email is required').isEmail(),  
         validationsMiddlewares.validateFields,       
-    ]
-, userController.create);
+    ], 
+    userController.create);
 
 router.get('/:id', userController.get);
 
 router.put('/:id',
     [
+        validationsMiddlewares.validateJWT,
         check('firstName', 'First name is required').not().isEmpty(),
         check('lastName', 'Last name is required').not().isEmpty(),
         check('dni', 'Dni is required').not().isEmpty(),
         check('password', 'Password is required').not().isEmpty(), 
         check('email', 'Email is required').isEmail(),
         validationsMiddlewares.validateFields,       
-    ]
-, userController.update);
+    ], 
+    userController.update);
 
-router.delete('/:id', userController.delete);
+router.delete('/:id',
+    [
+        validationsMiddlewares.validateJWT
+    ], 
+    userController.delete);
 
 module.exports = router;
