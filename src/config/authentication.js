d87f4c396cc1a46c624d2a2f0ebf8583156419f2
Nicolas <rojonicolasdev@gmail.com>
2023-05-16 15:34:25 -0300

    Added jwt sign

--- src/config/authentication.js
+++ src/config/authentication.js
@@ -20,4 +20,26 @@ const auth = async (req, res, next) => {
   }
 };
 
-module.exports = auth;
+const generateJWT = ( uid ) => {
+  return new Promise((resolve, reject) => {
+    const payload = {
+      uid
+    };
+    jwt.sign( payload, process.env.JWT_SECRET, {
+      expiresIn: '12h'
+    }, (err, token) => {  
+      if(err) {
+        console.log(err);
+        reject('Error generating JWT');
+      } else {
+        resolve(token);
+      }  
+    });
+
+  });
+};
+
+module.exports = {
+  auth,
+  generateJWT
+};
