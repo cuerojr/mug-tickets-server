1c11549a2a9e45d172d959df38673378d9c8b255
Nicolas <rojonicolasdev@gmail.com>
2023-07-22 12:51:55 -0300

    Minor fix

--- src/models/ticketModel.js
+++ src/models/ticketModel.js
@@ -66,7 +66,7 @@ ticketSchema.pre('save', async function (next) {
   if (!doc.ticketNumber) {
     try {
       const counter = await Counter.findByIdAndUpdate(
-        { _id: 'ticketNumber' },
+        { _id: this.event },
         { $inc: { sequence_value: 1 } },
         { new: true, upsert: true }
       ).exec();
