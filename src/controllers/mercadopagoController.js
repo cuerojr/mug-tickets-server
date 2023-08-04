5eed5cab8b1a8ded8e8ed28ba17975d372d71331
Nicolas <rojonicolasdev@gmail.com>
2023-08-04 11:05:37 -0300

    [ADDED] Mercadopago controller

---
+++ src/controllers/mercadopagoController.js
@@ -0,0 +1,43 @@
+const { response } = require('express');
+const { createTransaction, feedback } = require('../helpers/mercadopago');
+
+class MercadopagoController {    
+    constructor(){}
+
+    async create(req, res = response) {
+        const { email, password } = req.body;
+        try {
+            createTransaction(req.body);
+
+            res.status(200).json({
+                ok: true,
+                token
+            });
+        } catch (err) {
+            res.status(500).json({ 
+                ok: false, 
+                error: err.message 
+            });
+        }
+    }
+
+    async feedback(req, res = response) {
+        const { payment_id, status, merchant_order_id } = req.query;
+        try {
+            res.status(200).json({
+                Payment: payment_id,
+                Status: status,
+                MerchantOrder: merchant_order_id
+            });
+        } catch (err) {
+            res.status(500).json({ 
+                ok: false, 
+                error: err.message 
+            });
+        }
+    };
+}
+
+module.exports = {
+    MercadopagoController
+};
