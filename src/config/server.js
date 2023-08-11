bfedf90b5a656913dabfba24aa4309661a184b11
Nicolas <rojonicolasdev@gmail.com>
2023-08-11 09:13:33 -0300

    Minor fix

--- src/config/server.js
+++ src/config/server.js
@@ -35,7 +35,7 @@ class Server {
         this.app.use(function (req, res, next) {
             res.header('Access-Control-Allow-Origin', '*')
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, API-Key')
-            next()
+            next();
         });
     }
     
