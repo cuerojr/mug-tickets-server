75538e13ed13bdf9c44364c7ae4661a0786df15e
Nicolas <rojonicolasdev@gmail.com>
2023-10-02 11:57:56 -0300

    Minor fix

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -12,64 +12,64 @@ const pass = process.env.EMAIL_PASS;
  * @param {Array} tickets - An array of tickets to be included in the email.
  */
 export const sendMails = async (tickets = []) => {
-  console.log('sendmail', tickets)
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'mug.rosario@gmail.com',
-      pass: 'jkhn iusb wpat hsrq'
-    }
-  });
-  
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify((error, success) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-          } else {
-            console.log("Server is ready to take our messages");
-            resolve(success);
-          }
-        });
-      });
-      
-      const handlebarOptions = {
-        viewEngine: {
-          extName: '.handlebars',
-          partialsDir: path.resolve('./views'),
-          defaultLayout: false,
-        },
-        viewPath: path.resolve('./views'),
-        extName: '.handlebars'    
+  try {
+    console.log('sendmail', tickets)
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'mug.rosario@gmail.com',
+          pass: 'jkhn iusb wpat hsrq'
       }
-      
-      transporter.use('compile', hbs(handlebarOptions));
-      
-      const attachmentsFormated = [...tickets].map((ticket, index) => {
-        return {
-          filename: `ticket-${index}.png`,
-          path: ticket.qrCode,
-        }
-      });    
-      
-    try {
-      const mailOptions = {
-        from: 'mug.rosario@gmail.com',
-        to: tickets[0].purchaser.purchaserEmail,
-        subject: 'Entradas FestiMug',
-        template: 'email',
-        context: {
-          tickets
-        },
-        attachDataUrls: true,
-        attachments: attachmentsFormated,
-      };    
-      
-      const data = await transporter.sendMail(mailOptions);
-      console.log(data.response)
-      //transporter.close();
-    } catch (error) {
-      console.error(error)
+    });
+
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify((error, success) => {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+    });
+  
+    const handlebarOptions = {
+      viewEngine: {
+        extName: '.handlebars',
+        partialsDir: path.resolve('./views'),
+        defaultLayout: false,
+      },
+      viewPath: path.resolve('./views'),
+      extName: '.handlebars'    
     }
+    
+    transporter.use('compile', hbs(handlebarOptions));
+  
+    const attachmentsFormated = [...tickets].map((ticket, index) => {
+      return {
+        filename: `ticket-${index}.png`,
+        path: ticket.qrCode,
+      }
+    });    
+  
+    const mailOptions = {
+      from: 'mug.rosario@gmail.com',
+      to: tickets[0].purchaser.purchaserEmail,
+      subject: 'Entradas FestiMug',
+      template: 'email',
+      context: {
+        tickets
+      },
+      attachDataUrls: true,
+      attachments: attachmentsFormated,
+    };    
+  
+    const data = await transporter.sendMail(mailOptions);
+    console.log(data.response)
+    //transporter.close();
+  } catch (error) {
+    console.error(error)
   }
+}
